cb6e6e0b473802c0768f64560e1091e5282a2750
Alesander <alesanderlg@gmail.com>
2018-10-15 21:18:01 -0300

    classe de teste

--- kata/src/test/java/kata/testClass.java
+++ kata/src/test/java/kata/testClass.java
@@ -18,7 +18,85 @@ public class testClass
 {
 
     @Test
-    public void test()
+    public void cartaAlta_deveRetornaMaiorValor() 
+    {
+    		fail("Not yet implemented");
+    }
+    
+    @Test
+    public void par_deveRetornaDuasCartasMesmoValor() 
+    {
+    		fail("Not yet implemented");
+    }
+    
+    @Test
+    public void doisPares_deveRetornaDoisParesDiferentes() 
+    {
+    		fail("Not yet implemented");
+    }
+    
+    @Test
+    public void trinca_deveRetornaTresCartasMesmoValorEDuasValoresDiferentes() 
+    {
+    		fail("Not yet implemented");
+    }
+    
+    @Test
+    public void straight_deveRetornarTodasAsCartasComValoresConsecutivos()
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void flush_deveRetornarTodasAsCartasComMesmoNaipe()
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void fullHouse_deveRetornarUmaTrincaEUmPar()
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void quadra_deveRetornarQuatroCartasMesmoValor()
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void straightFlush_deveRetornarTodasCartasConsecutivasEMesmoNaipe()
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void royalFlush_deveRetornarSequencia10ValeteDamaReisAsDoMesmoNaipe()
+    {
+        fail("Not yet implemented");
+    }
+    
+    /*
+     * ordem crescente de valor: 2, 3, 4, 5, 6, 7, 8, 9, 10, Valete, Dama, Rei, Ás.
+     * */
+    @Test
+    public void ordenacaoCartas_deveRetornarNaOrdemCrescente() 
+    {
+        fail("Not yet implemented");
+    }
+    
+    /*
+     * Os naipes são: Ouro (D), Copa (H), Espadas (S), Paus (C)
+     * */
+    @Test
+    public void naipes_deveRetornarOuroCopasEspadaPaus() 
+    {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void jogadresMesmaMao_deveRetornarVencedorComCartasDeMaiorValor() 
     {
         fail("Not yet implemented");
     }
